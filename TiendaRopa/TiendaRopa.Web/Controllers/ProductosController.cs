a5c0ca611ed85e4e2bbe9ddcd2c7ca33a7dbd554
ANA GABRIELA MEJIA MARTINEZ <agmejiam@unah.hn>
2021-03-17 14:17:52 -0600

    agrgando arquitectura basica

--- TiendaRopa/TiendaRopa.Web/Controllers/ProductosController.cs
+++ TiendaRopa/TiendaRopa.Web/Controllers/ProductosController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TiendaRopa.Web.Models;
+using Tiendaropa.BL;
 
 namespace TiendaRopa.Web.Controllers
 {
@@ -12,32 +12,8 @@ namespace TiendaRopa.Web.Controllers
         // GET: Produtos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Camisa de Talla M";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pantalon Jean Azul Talla 32";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Blusa Roja Talla S";
-
-            var producto4 = new ProductoModel();
-            producto4.Id = 4;
-            producto4.Descripcion = "Pantalon de Tela Talla 30";
-
-            var producto5 = new ProductoModel();
-            producto5.Id = 5;
-            producto5.Descripcion = "Tenis Nike Talla 9";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
-            listadeProductos.Add(producto5);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
---
+++ TiendaRopa/Tiendaropa.BL/ProductosBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendaropa.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Camisa de Talla M";
+            producto1.Precio = 200;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pantalon Jean Azul Talla 32";
+            producto2.Precio = 700;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Blusa Roja Talla S";
+            producto3.Precio = 600;
+
+            var producto4 = new Producto();
+            producto4.Id = 4;
+            producto4.Descripcion = "Pantalon de Tela Talla 30";
+            producto4.Precio = 500;
+
+            var producto5 = new Producto();
+            producto5.Id = 5;
+            producto5.Descripcion = "Tenis Nike Talla 9";
+            producto5.Precio = 1200;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            listadeProductos.Add(producto4);
+            listadeProductos.Add(producto5);
+
+            return listadeProductos; 
+        }
+    }
+}
